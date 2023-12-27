7de303114041ee6af0db6a058ac6b0e33a4dc927
Anna Kuo <juice1114guo@gmail.com>
2023-12-27 22:00:00 +0800

    調整：header的li

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -5,11 +5,14 @@ import { KnifeFork } from '@element-plus/icons-vue';
 <template>
     <div class="bg-[url('/img/header_desktop.png')] bg-cover bg-center h-[420px]">
         <div class="flex justify-between items-center px-10 py-7 bg-black/[0.88] text-white h-[56px] ">
-            <div class="text-lg font-black">六角西餐廳</div>
+            <div class="text-lg font-black">小咪西餐廳</div>
             <div class="flex gap-3">
-                <NuxtLink :to="{ name: 'index' }">特色</NuxtLink>
+                <NuxtLink :to="{ name: 'index' }">關於我們</NuxtLink>
                 <NuxtLink :to="{ name: 'about' }" style="margin-left: 10px">線上訂購</NuxtLink>
-                <NuxtLink :to="{ name: 'about' }" style="margin-left: 10px">地圖</NuxtLink>
+                <NuxtLink :to="{ name: 'member' }" style="margin-left: 10px">提供服務</NuxtLink>
+                <NuxtLink :to="{ name: 'member' }" style="margin-left: 10px">最新消息</NuxtLink>
+                <NuxtLink :to="{ name: 'member' }" style="margin-left: 10px">美味據點</NuxtLink>
+                <NuxtLink :to="{ name: 'reserve' }" style="margin-left: 10px">訂位資訊</NuxtLink>
             </div>
         </div>
         <div class="bg-black/[0.3] w-[460px] h-[152px] mx-auto my-auto relative top-[20%] p-5 text-center text-white">
