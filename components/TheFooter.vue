28848ce4aeae0b6389234bd3a1dba2892fc2b2ef
Anna Kuo <juice1114guo@gmail.com>
2023-12-27 22:02:33 +0800

    調整：footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,26 +1,26 @@
 <script setup>
+
 </script>
 
 <template>
-    <div class="bg-[#efeae7] h-[200px] flex justify-between py-8 px-11 ">
-        <div class="flex gap-5 w-8/12">
-            <div class="w-1/2">
-                <div class="leading-loose text-xl font-semibold">關於 六角西餐廳</div>
-                <p>Curabitur lobortis id lorem id bibndum. Ut id consectetur magna. Quisque volutpat augue enim, pulvinar
-                    lobortis nibh lacinia at. </p>
-            </div>
-            <div class="w-1/3">
-                <div class="leading-loose text-xl font-semibold">聯絡方式</div>
-                <ul>
-                    <li>• 高雄市中正五路到底</li>
-                    <li>• +886 886 886</li>
-                    <li>• service@hexschool.com</li>
-                </ul>
-            </div>
+    <div class="bg-primary h-[200px] flex justify-start py-8 px-11 max-md:px-2">
+
+        <div class="w-1/3">
+            <div class="leading-loose text-xl font-semibold">關於 小咪西餐廳</div>
+            <p>Curabitur lobortis id lorem id bibndum. Ut id consectetur magna. Quisque volutpat augue enim, pulvinar
+                lobortis nibh lacinia at. </p>
+        </div>
+        <div class="w-1/3">
+            <div class="leading-loose text-xl font-semibold">聯絡方式</div>
+            <ul>
+                <li>• 高雄市中正五路到底</li>
+                <li>• +886 886 886</li>
+                <li>• service@hexschool.com</li>
+            </ul>
         </div>
 
-        <div>
-            <div class="text-[#3D1101] text-4xl font-extrabold">六角西餐廳</div>
+        <div class="w-1/3">
+            <div class="text-amber-950 text-5xl font-extrabold max-md:text-4xl">小咪西餐廳</div>
         </div>
     </div>
 </template>
