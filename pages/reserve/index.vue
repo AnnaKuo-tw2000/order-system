d9e3982f62c5e3e98dd6ebf080d48acaabb2c73c
Anna Kuo <juice1114guo@gmail.com>
2024-01-02 15:08:30 +0800

    調整：訂位頁圖片寬度、右半部input寬度

--- pages/reserve/index.vue
+++ pages/reserve/index.vue
@@ -7,13 +7,13 @@ const radio1 = ref('1');
 
 <template>
     <section class="bg-[url('/img/bg.jpg')] p-10 text-amber-950 max-md:p-6">
-        <div class="text-center text-xl mb-5">我們的位子不太夠坐要不要先訂位呢？</div>
-        <div class="flex max-md:gap-6">
-            <div class="w-1/2">
+        <div class="text-center text-3xl mb-10">我們的位子不太夠坐要不要先訂位呢？</div>
+        <div class="flex justify-center gap-10">
+            <div class="w-auto">
                 <img src="../../img/Map.png" alt="" class=" h-full">
             </div>
 
-            <div class="flex flex-col w-1/2 gap-1">
+            <div class="flex flex-col w-[30%] gap-1">
                 <p>姓名</p>
                 <el-input v-model="input" placeholder="" />
 
@@ -28,8 +28,8 @@ const radio1 = ref('1');
 
                 <p class="mt-2">是否需要素食</p>
                 <el-radio-group v-model="radio1" class="relative bottom-2">
-                    <el-radio label="1" size="large">Option 1</el-radio>
-                    <el-radio label="2" size="large">Option 2</el-radio>
+                    <el-radio label="1" size="large">是</el-radio>
+                    <el-radio label="2" size="large">否</el-radio>
                 </el-radio-group>
 
                 <el-row class=" self-end">
