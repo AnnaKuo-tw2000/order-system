a201eab165de173f83709246ec3acf2c6c002cb2
Anna Kuo <juice1114guo@gmail.com>
2024-01-10 14:24:17 +0800

    新增：篩選產品邏輯

--- pages/order/UserOrderPage.vue
+++ pages/order/UserOrderPage.vue
@@ -1,11 +1,8 @@
 <script setup>
-// import { useAuthStore } from '@/stores/auth';
-
-// const authStore = useAuthStore();
 import * as firebaseDb from "firebase/database";
-import * as firebaseAuth from "firebase/auth";
+import { useAuthStore } from '@/stores/auth';
 
-const auth = firebaseAuth.getAuth();
+const authStore = useAuthStore();
 const db = firebaseDb.getDatabase();
 
 // 訂餐列表
@@ -30,42 +27,73 @@ const open_loginAlertDialog = ref(false);
 // 添加至購物車
 const cartStore = useCartStore();
 function addToShoppingCart(food) {
-    firebaseAuth.onAuthStateChanged(auth, (user) => {
-        // 如果有人登錄才能加入購物車
-        let num = 1;
-        if (user.uid) {
-            if (cartStore.cartList) {
-                console.log('有東西');
-                // 同樣的東西
-                const targetFood = cartStore.cartList.find((item) => item.uid === food.uid);
-                if (targetFood) { num = targetFood.num + 1; }
-            }
-            // 不同的東西直接加
-            console.log('沒東西');
-            firebaseDb.set(firebaseDb.ref(db, `userInfo/${user.uid}/shoppingCart/${food.uid}`), {
-                ...food,
-                num
-            });
-        } else {
-            open_loginAlertDialog.value = true;
+    // 如果有人登錄才能加入購物車
+    let num = 1;
+    if (authStore.userInfo?.uid) {
+        if (cartStore.cartList) {
+            console.log('有東西');
+            // 同樣的東西
+            const targetFood = cartStore.cartList.find((item) => item.uid === food.uid);
+            if (targetFood) { num = targetFood.num + 1; }
+        }
+        // 不同的東西直接加
+        console.log('沒東西');
+        firebaseDb.set(firebaseDb.ref(db, `userInfo/${authStore.userInfo.uid}/shoppingCart/${food.uid}`), {
+            ...food,
+            num
+        });
+    } else {
+        open_loginAlertDialog.value = true;
+    }
+}
+
+// 篩選商品
+const filterFood = ref('all');
+const setFilter = (type) => {
+    filterFood.value = type;
+};
+const filterFoodList = computed(() => {
+    const foodsArray = Object.values(foodList.value);
+
+    return foodsArray.filter((food) => {
+        switch (filterFood.value) {
+            case "salad":
+                return food.type === '沙拉';
+            case "snack":
+                return food.type === '小點';
+            case "main course":
+                return food.type === '主餐';
+            case "dessert":
+                return food.type === '甜點';
+            default:
+                return foodsArray;
         }
     });
-}
+});
+
 </script>
 
 <template>
-    <div class="bg-[url('/img/bg.jpg')]  p-12 flex justify-center gap-10 text-amber-950 w-full">
+    <div class="bg-[url('/img/bg-1.jpg')]  p-12 flex justify-center gap-10 text-amber-950 w-full">
         <div class="w-1/5">
             <div class="text-3xl leading-[3] text-center">線上訂購美食</div>
             <ul class="text-xl">
-                <li class="border-b border-black leading-loose hover:bg-amber-950  hover:text-white cursor-pointer">全部(23)
+                <li @click="setFilter('all')"
+                    class="border-b border-black leading-loose hover:bg-amber-950  hover:text-white cursor-pointer">全部餐點
                 </li>
-                <li class="border-b border-black leading-loose  hover:bg-amber-950  hover:text-white cursor-pointer">
-                    只愛吃薯條(18)</li>
-                <li class="border-b border-black leading-loose  hover:bg-amber-950  hover:text-white cursor-pointer">
-                    加熱就可食(1)</li>
-                <li class="border-b border-black leading-loose  hover:bg-amber-950  hover:text-white cursor-pointer">
-                    不敢漢堡系列(4)</li>
+                <li @click="setFilter('main course')"
+                    class="border-b border-black leading-loose  hover:bg-amber-950  hover:text-white cursor-pointer">
+                    精緻主餐</li>
+                <li @click="setFilter('snack')"
+                    class="border-b border-black leading-loose  hover:bg-amber-950  hover:text-white cursor-pointer">
+                    精選小點</li>
+                <li @click="setFilter('salad')"
+                    class="border-b border-black leading-loose  hover:bg-amber-950  hover:text-white cursor-pointer">
+                    營養沙拉</li>
+                <li @click="setFilter('dessert')"
+                    class="border-b border-black leading-loose  hover:bg-amber-950  hover:text-white cursor-pointer">
+                    甜蜜蜜甜點</li>
+
             </ul>
         </div>
         <!-- 訂餐列表 -->
@@ -74,7 +102,7 @@ function addToShoppingCart(food) {
                 訂餐列表
             </h1>
             <div class="flex flex-wrap px-2 w-full gap-6">
-                <div v-for="food in foodList" :key="food.uid"
+                <div v-for="food in filterFoodList" :key="food.uid"
                     class="flex flex-col w-[250px] h-[300px]  items-center shadow-lg text-center">
                     <img :src="food.imageUrl" alt="" class="mb-5 w-[100px] h-[100px] object-cover">
                     <div class="text-2xl leading-loose font-semibold">{{ food.title }}</div>
