b763ea46d25d0a407cbc75d8c43890e8013280cc
Anna Kuo <juice1114guo@gmail.com>
2024-01-10 14:25:14 +0800

    新增：刪除及更新購物車邏輯； 調整：nav路由調整

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,13 +1,26 @@
 <script setup>
 import * as firebaseAuth from "firebase/auth";
+import * as firebaseDb from "firebase/database";
 import { useAuthStore } from '@/stores/auth';
 
-const num = ref(1);
 const auth = firebaseAuth.getAuth();
 const authStore = useAuthStore();
+const cartStore = useCartStore();
+const db = firebaseDb.getDatabase();
 
 function logOut() {
     firebaseAuth.signOut(auth);
+}
+
+// 刪除購物車
+function deleteFood(food) {
+    firebaseDb.remove(firebaseDb.ref(db, `userInfo/${authStore.userInfo.uid}/shoppingCart/${food.uid}`));
+}
+// 更新購物車
+function updateShoppingCart(num, food) {
+    firebaseDb.update(firebaseDb.ref(db, `userInfo/${authStore.userInfo.uid}/shoppingCart/${food.uid}`), {
+        num
+    });
 }
 </script>
 
@@ -15,7 +28,7 @@ function logOut() {
     <nav class="fixed top-0 flex justify-between items-center px-10 text-white h-[70px] z-50 w-full">
         <div class="flex items-center">
             <h1 class="text-3xl font-black">小咪西餐廳</h1>
-            <img src="../img/cat2.png" alt="" class="h-[55px] ml-1" />
+            <img src="../img/logo.png" alt="" class="h-[55px] ml-1" />
         </div>
         <ul class="flex h-full text-xl">
             <li class="h-full mx-4">
@@ -26,10 +39,10 @@ function logOut() {
                 <NuxtLink :to="{ name: 'order-UserOrderPage' }" class="h-full flex items-center">線上訂購</NuxtLink>
             </li>
             <li class="h-full mx-4">
-                <NuxtLink :to="{ name: 'auth-register' }" class="h-full flex items-center">訂位資訊</NuxtLink>
+                <NuxtLink :to="{ name: 'order-AdminOrderPage' }" class="h-full flex items-center">管理訂餐</NuxtLink>
             </li>
             <li class="h-full mx-4" v-if="!authStore.userInfo">
-                <NuxtLink :to="{ name: 'example-auth-login' }" class="h-full flex items-center">登入</NuxtLink>
+                <NuxtLink :to="{ name: 'auth-login' }" class="h-full flex items-center">登入</NuxtLink>
             </li>
             <li class="h-full mx-4" @click="logOut" v-else>
                 <span class="h-full flex items-center">登出</span>
@@ -43,46 +56,35 @@ function logOut() {
                     </li>
                 </template>
                 <template #default>
-                    <div class="p-4 ">
-                        <div class="flex flex-col gap-3 mb-3">
+                    <div class="p-4 " v-if="cartStore.cartList">
+                        <div v-for="food in cartStore.cartList" :key="food.uid" class="flex flex-col gap-3 mb-3">
                             <div class="flex gap-3">
-                                <img src="..//img/food10.jpg" alt="" class="w-1/4 h-1/4">
+                                <img :src="food.imageUrl" alt="" class="w-1/4 h-1/4">
                                 <div>
-                                    <p class="text-lg">經典漢堡套餐</p>
-                                    <p>Classic Hamburger Package</p>
-                                    <p class="text-red-600 mb-2">$120</p>
+                                    <p class="text-lg">{{ food.title }}</p>
+                                    <p class="text-red-600 mb-2">${{ food.price }}</p>
                                     <div class="flex items-center justify-between ">
-                                        <el-input-number v-model="num" :min="1" :max="10" />
-                                        <font-awesome-icon :icon="['far', 'trash-can']" class="hover:cursor-pointer" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex gap-3">
-                                <img src="..//img/food10.jpg" alt="" class="w-1/4 h-1/4">
-                                <div>
-                                    <p class="text-lg">經典漢堡套餐</p>
-                                    <p>Classic Hamburger Package</p>
-                                    <p class="text-red-600 mb-2">$120</p>
-                                    <div class="flex items-center  justify-between">
-                                        <el-input-number v-model="num" :min="1" :max="10" />
-                                        <font-awesome-icon :icon="['far', 'trash-can']" class="hover:cursor-pointer" />
+                                        <el-input-number v-model="food.num" :min="1" :max="10"
+                                            @change="updateShoppingCart($event, food)" />
+                                        <font-awesome-icon :icon="['far', 'trash-can']" @click="deleteFood(food)"
+                                            class="ml-4 hover:cursor-pointer" />
                                     </div>
                                 </div>
                             </div>
                         </div>
 
                         <div class="border-t-2 flex flex-col gap-3 py-2">
-                            <p class="text-lg">購物車中有2品項</p>
+                            <p class="text-lg">購物車中有{{ cartStore.cartList.length }}品項</p>
                             <div class="flex justify-between">
                                 <p>合計</p>
-                                <p class="text-red-600">NT$240</p>
+                                <p class="text-red-600">NT${{ cartStore.cartSum }}</p>
                             </div>
                             <el-button type="primary" round class="w-[60%] self-center">結賬</el-button>
                         </div>
                     </div>
+                    <div v-else class="text-center">購物車中沒有商品</div>
                 </template>
             </el-popover>
-
         </ul>
     </nav>
 </template>
