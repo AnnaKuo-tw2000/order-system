3633c759529dd0b92c2b063e04edd1b039eaa34b
Anna Kuo <juice1114guo@gmail.com>
2024-01-24 22:08:18 +0800

    Updates

---
+++ static/cart.8a1a5f3a.js
@@ -0,0 +1,1 @@
+import{B as a,r as n,C as o}from"./entry.7c31a8c4.js";const c=a("cart",()=>{const r=n(null),t=o(()=>r.value?r.value.reduce((u,e)=>u+e.price*e.num,0):0);return{cartList:r,cartSum:t}});export{c as u};
---
+++ static/logo.465f8b70.js
@@ -0,0 +1,1 @@
+import"./entry.7c31a8c4.js";const r=""+new URL("logo.21325f73.png",import.meta.url).href;export{r as _};
